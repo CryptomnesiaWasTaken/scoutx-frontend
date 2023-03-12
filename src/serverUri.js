b087d96460f165227698b127699dd71cb93d5a45
Cryptomnesia <82593272+CryptomnesiaWasTaken@users.noreply.github.com>
2023-03-12 01:22:31 +0100

    Update serverUri.js

--- src/serverUri.js
+++ src/serverUri.js
@@ -1,3 +1,3 @@
 export default process.env.NODE_ENV === "development"
-	? "https://scoutx.onrender.com/"
-	: "https://scoutx.onrender.com/";
+	? "https://scoutx.onrender.com"
+	: "https://scoutx.onrender.com";
